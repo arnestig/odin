5a28e4eaec97e43a4533de21207eb4decce87b4d
Martin Rydin <martin.rydin@gmail.com>
2016-07-08 08:54:32 +0200

    WIP. Only labelling works currently...

--- www/manage_settings.php
+++ www/manage_settings.php
@@ -6,11 +6,14 @@ include_once('include/html_frame.php');
 include_once('include/settings.php');
 
 $settings = new Settings();
-$cur_settings = $settings->getSettings();
 $available_groups = $settings->getSettingGroups();
+$cur_s_group = $available_groups[0];
+print_r($cur_s_group);
+$cur_settings = $settings->getSettings($cur_s_group['sg_name']);
+
 
 // TODO: set from GET-req 
-$cur_s_group = $available_groups[0];
+
 
 if ( isset( $_POST[ 'submit' ] ) ) {
   $settingsmax = $_POST[ 'settingsIdMax' ];
@@ -57,63 +60,27 @@ function displaySettings($s_group) {
                       <input type="'.$cursetting[ 's_type' ].'" class="form-control" id="'.$cursetting[ 's_name' ].'" ';
     if ($cursetting[ 's_type' ] === 'checkbox' && $cursetting[ 's_value' ] === '1') {
       $form_html .= 'checked';
+    } else {
+      $form_html .= 'value="'.$cursetting_value.'"';
     }
-                         '.checked_box($cur_settings[ 'email_notification' ]).'>
+    $form_html.= '>
                     </div>
                   </div>';
   }
-            
-
-
-    echo '<tr data-toggle="tooltip" title="'.$cursetting[ 's_description' ].'">
-      <td>'.$cursetting[ 's_fullname' ].'</td>';
-    if ( $cursetting[ 's_type' ] === 'text' ) {
-      echo '<td><INPUT type="text" name="dsValue'.$settingid.'" value="'.$cursetting[ 's_value' ].'">';
-    } elseif( $cursetting[ 's_type' ] === 'bool' ) {
-      echo '<td><INPUT type="checkbox" name="dsValue'.$settingid.'"';
-      if ( $cursetting[ 's_value' ] === '1' ) {
-        echo ' checked';
-      }
-      echo '>';
-    }
-    echo '<INPUT type="hidden" name="dsName'.$settingid.'" value="'.$cursetting[ 's_name' ].'">';
-    echo '<INPUT type="hidden" name="dsType'.$settingid.'" value="'.$cursetting[ 's_type' ].'"></td></tr>';
-    $settingid++;
-  }
-  echo '<tr><td align="right" colspan=2>
-      <BUTTON type="submit" name="dsSubmit" value="Save">Save</BUTTON>
-      <BUTTON type="submit" name="dsSubmit" value="Cancel">Cancel</BUTTON>
-      <INPUT type="hidden" name="dsSettingsIdMax" value="'.$settingid.'">
-      </table></FORM>';
+  $form_html .= '<div class="form_group">
+                  <div="col-lg-12">
+                    <input type="hidden" name="settingsIdMin" value="">
+                    <input type="hidden" name="settingsIdMax" value="">
+                  </div>
+                </div>
+              </form>';
+  return $form_html;
 }
 
 $frame = new HTMLframe();
 $frame->doc_start("Configure Settings");
 $frame->doc_nav("Settings", $_SESSION[ 'user_data' ][ 'usr_usern' ] );
 
-
-
-
-
-
-
-//TODO
-function genSettings() {
-  foreach ($cur_settings as $setting_row) {
-    // Ta ut rubriker Och lägg i eǵen array
-  }
-  
-  foreach ($variable as $key => $value) {
-    # code...
-  }
-}
-
-function checked_box($cb_name) { 
-  if($cur_settings[$cb_name] !== 0) {
-    return 'checked';
-  }
-  return '';
-}
 
 echo '
     <div class="container">
@@ -132,56 +99,7 @@ echo '
             </div>
           </div>
           
-
-          
-
-            <div>
-              <hr>
-            </div>
-
-            <div class="form-group">
-              <label for="scanHostsInterval" class="col-lg-6 control-label">Scan hosts interval</label>
-              <div class="col-lg-6">
-                <input type="text" class="form-control" id="scanHostsInterval" placeholder="Enter hours" value="">
-              </div>
-            </div>
-            <div class="form-group">
-              <label for="leaseTime" class="col-lg-6 control-label">Lease time</label>
-              <div class="col-lg-6">
-                <input type="text" class="form-control" id="leaseTime" placeholder="Enter days" value="365">
-              </div>
-            </div>
-
-            <div>
-              <hr>
-            </div>
-            <div class="form-group">
-              <label for="orgName" class="col-lg-6 control-label">Organization name</label>
-              <div class="col-lg-6">
-                <input type="text" class="form-control" id="orgName" placeholder="Enter your organizations name" value="Odin">
-              </div>
-            </div>
-            <div class="form-group">
-              <label for="logoUploadFile" class="col-lg-6 control-label">Upload your logo</label>
-              <div class="col-lg-6">
-                <input type="file" id="logoUploadFile">
-                <p class="help-block">Make the size 30px in height and 60 in width. Format should be .png.</p>
-              </div>
-            </div>
-
-            <div>
-              <hr>
-            </div>
-            
-            <div class="form-group">
-              <div class="col-lg-offset-8 col-lg-2">
-                <button type="submit" class="btn btn-default">Cancel</button>
-              </div>
-              <div class="col-lg-2">
-                <button type="submit" class="btn btn-default">Save</button>
-              </div>
-            </div>
-          </form>
+          '.displaySettings($cur_s_group).'
 
         </div>
 
