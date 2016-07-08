d0a129dca97553f1720dc88834b1166afd2f34f1
Tobias Eliasson <arnestig@gmail.com>
2016-07-08 13:39:10 +0200

    - Added from_usr_id field in notify table

--- database/70-notifyusers.sql
+++ database/70-notifyusers.sql
@@ -5,6 +5,7 @@ alter sequence sq_notify_id owner to dbaodin;
 
 create table notifyusers (
         nu_id integer primary key default nextval('sq_notify_id'),
+        nu_sent_by_id smallint not null references users default 0,
         nu_usr_id smallint not null references users default 0,
         nu_notification_sent boolean default false,
         nu_message text
@@ -17,11 +18,12 @@ alter table notifyusers owner to dbaodin;
 create or replace function notify_user(
     ticket varchar(255),
     usr_id smallint,
-    message text )
+    message text,
+    from_usr_id smallint default 0)
 returns void AS $$
 begin
-    INSERT INTO notifyusers( nu_usr_id, nu_message ) VALUES ( usr_id, message );
+    INSERT INTO notifyusers( nu_usr_id, nu_sent_by_id, nu_message ) VALUES ( usr_id, from_usr_id, message );
 end;
 $$ language plpgsql;
-alter function notify_user(varchar,smallint,text) owner to dbaodin;
+alter function notify_user(varchar,smallint,text,smallint) owner to dbaodin;
 
