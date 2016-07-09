47e01f869d6c6ddc855bb299965b722c0f2365cb
Martin Rydin <martin.rydin@gmail.com>
2016-07-09 05:56:14 +0200

    login error messages implemented

--- www/index.php
+++ www/index.php
@@ -5,18 +5,30 @@ include_once( "include/user.php" );
 include_once( "include/settings.php" );
 include_once( "include/html_frame.php" );
 
-if (!empty($_POST['username']) && !empty($_POST['password'])) {
-  $user = new User();
-  $name = $_POST['username'];
-  $pwd = $_POST['password'];
-  if ($user->login($name,$pwd)) {
-    if ($_SESSION[ 'user_data' ][ 'server_gen_pwd' ] === 1) {
-      header('Location: change_password.php');
+$alert_msg = '';
+$alert_type = '';
+
+if ( isset($_POST[ 'submit' ]) && !empty($_POST[ 'submit' ]) ) {
+  if (!empty($_POST['username']) && !empty($_POST['password'])) {
+    $user = new User();
+    $name = $_POST['username'];
+    $pwd = $_POST['password'];
+    if ($user->login($name,$pwd)) {
+      if ($_SESSION[ 'user_data' ][ 'server_gen_pwd' ] === 1) {
+        header('Location: change_password.php');
+      } else {
+        header('Location: overview.php');  
+      }  
     } else {
-      header('Location: overview.php');  
-    }  
+      $alert_msg = 'No user with given credentials exist.';
+      $alert_type = 'danger';
+    }
+  } else {
+    $alert_msg = 'Please fill out both fields before continuing.';
+    $alert_type = 'warning';
   }
 }
+
 
 function userRegistration() {
   $settings = new Settings();
@@ -32,6 +44,16 @@ function userRegistration() {
         </div>';
       }
   }
+}
+
+function alert($alert_msg, $alert_type) {
+  if ( empty($alert_msg) && empty($alert_type) ) return '';
+  return '
+  <div class="form-group row">
+    <div class="col-lg-12 alert alert-'.$alert_type.'">
+      '.$alert_msg.'
+    </div>
+  </div>';
 }
 
 $frame = new HTMLframe();
@@ -51,21 +73,28 @@ echo '
       </div>
       <div class="row">
         <div class="col-lg-offset-4 col-lg-4">
-          <form id="login" name="login_form" action="" method="post">
-            <div class="form-group">
-              <label for="username">Username</label>
-              <input type="text" class="form-control" id="username" name="username" placeholder="Username" autocomplete="on">
+          <form class="form" id="login_form" name="login_form" action="index.php" method="post">
+            <div class="form-group row">
+              <div class="col-lg-12">
+                <label for="username">Username</label>
+                <input type="text" class="form-control" id="username" name="username" placeholder="Username" autocomplete="on">
+              </div>
             </div>
-            <div class="form-group">
-              <label for="password">Password</label>
-              <input type="password" class="form-control" id="password" name="password" placeholder="Password">
-            </div>
-            <div class="checkbox">
-              <label>
+            <div class="form-group row">
+              <div class="col-lg-12">
+                <label for="password">Password</label>
+                <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+              </div>
+            </div>'
+            .alert($alert_msg, $alert_type).
+            '<div class="form-group row">
+              <div class="col-lg-6">
                 <input type="checkbox" id="login-chkbx" name="login-chkbx">Remember me
-              </label>
+              </div>
+              <div class="col-lg-6">
+                <input type="submit" name="submit" value="Log in" class="btn btn-info pull-right">
+              </div>
             </div>
-            <button type="submit" name="submit" value="Submit" class="btn btn-default" id="login-btn">Log in</button>
           </form>
         </div>
       </div>';
