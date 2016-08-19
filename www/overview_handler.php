a16e553a3ccfb42379bb03eff997b5d38b53c415
Martin Rydin <martin.rydin@gmail.com>
2016-08-19 03:54:18 +0200

    WIP on unreserve host. Bug in database reactivates expired reservations when new host is added by client

--- www/overview_handler.php
+++ www/overview_handler.php
@@ -17,18 +17,23 @@ if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQU
 
 */
 
-if (isset($_POST[ 'checkbox' ]) && !empty($_POST[ 'checkbox' ])) {
+if (!empty($_POST[ 'ip' ]) && !empty($_POST[ 'action' ])) {
 	
-    $ip = $_POST[ 'checkbox' ];
+    $ip = $_POST[ 'ip' ];
+    $action = $_POST[ 'action' ];
     $nw_manager = new NetworkManagement();
-    $cur_reservations = $nw_manager->getReserved( $_SESSION[ 'user_data' ][ 'usr_id' ] );
-
-
-    if (!in_array( $ip, $cur_reservations ) ) {
-        echo $nw_manager->reserveHost( $ip, $_SESSION[ 'user_data' ][ 'usr_id' ] );
+    $status = '';
+    if ($action === 'true') {
+        $status = $nw_manager->reserveHost( $ip, $_SESSION[ 'user_data' ][ 'usr_id' ] );
     } else {
-        echo $nw_manager->unReserveHost( $ip, $_SESSION[ 'user_data' ][ 'usr_id' ] );
+        $nw_manager->unReserveHost( $ip, $_SESSION[ 'user_data' ][ 'usr_id' ] );
+        $status = true;
     }
+    //$return['reserved'][] = $nw_manager->getReserved( $_SESSION[ 'user_data' ][ 'usr_id' ] );
+    //$return['reply'] = $status;
+    //echo json_encode($return);
+    echo json_encode( array('opStatus' => $status, 'ipList' => $nw_manager->getReserved( $_SESSION[ 'user_data' ][ 'usr_id' ] )) );
+
 }
 if (isset($_POST[ 'getReserved' ]) && !empty($_POST[ 'getReserved' ])) {
 
