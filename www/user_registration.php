d1ec379c275a7d7f3072b8b30be61f7ac7e897a1
Tobias Eliasson <arnestig@gmail.com>
2016-09-12 15:19:51 +0200

    - Last commit to #23, forgot to add this file in last commit

--- www/user_registration.php
+++ www/user_registration.php
@@ -8,7 +8,7 @@ include_once('include/user.php');
 include_once('include/settings.php');
 
 $settings = new Settings();
-$user_reg = $settings->getSettingValue('user_registration');
+$user_reg = $settings->getSettingValue('allow_user_registration');
 if ($user_reg != 'checked') {
   header('Location: index.php');
 }
@@ -75,7 +75,7 @@ echo '
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="index.php"><img src="images/ODIN.png" alt="Odin - Logo"></a>
+          <a class="navbar-brand" href="index.php"><img src="logo.php?small" alt="Odin - Logo"></a>
         </div>
         <div id="navbar" class="collapse navbar-collapse navbar-right">
           <ul class="nav navbar-nav">
