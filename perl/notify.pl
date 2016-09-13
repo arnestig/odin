1f6400bc4fc8ec13ee05e85d1de21810b411636b
Tobias Eliasson <arnestig@gmail.com>
2016-09-13 08:50:43 +0200

    - Updated license information and copyright

--- perl/notify.pl
+++ perl/notify.pl
@@ -1,5 +1,26 @@
 #!/usr/bin/perl
 
+#  Odin - IP plan management and tracker
+#  Copyright (C) 2015-2016  Tobias Eliasson <arnestig@gmail.com>
+#                           Jonas Berglund <jonas.jberglund@gmail.com>
+#                           Martin Rydin <martin.rydin@gmail.com>
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License along
+#  with this program; if not, write to the Free Software Foundation, Inc.,
+#  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+*/
+
 use strict;
 use warnings;
 
@@ -47,7 +68,7 @@ sub get_setting
 
 sub send_email
 {
-    my($email_address,$fullname,$reenable_key,$host_ip,$host_description,$host_last_seen = @_;
+    my($email_address,$fullname,$reenable_key,$host_ip,$host_description,$host_last_seen) = @_;
     my $smtp = Net::SMTP->new( $::settings{ 'email_server_hostname' },
                               Port => $::settings{ 'email_server_port' });
     $smtp->mail('target-email');
