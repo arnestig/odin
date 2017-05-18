c3fc24a49d384ebb7d2b43e98f619b6b8a7429e8
Tobias Eliasson <arnestig@gmail.com>
2017-05-18 10:06:44 +0200

    - Updated copyright years

--- database/30-settings.sql
+++ database/30-settings.sql
@@ -1,6 +1,6 @@
 /*
    Odin - IP plan management and tracker
-   Copyright (C) 2015-2016  Tobias Eliasson <arnestig@gmail.com>
+   Copyright (C) 2015-2017  Tobias Eliasson <arnestig@gmail.com>
                             Jonas Berglund <jonas.jberglund@gmail.com>
                             Martin Rydin <martin.rydin@gmail.com>
 
